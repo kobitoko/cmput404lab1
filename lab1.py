1c98bcd36d86514b0a42165ba4d25d43228b87f8
kobitoko <kobitoko@gmail.com>
2016-09-08 14:56:20 -0600

    lab 1 + silly extra prints

---
+++ lab1.py
@@ -0,0 +1,17 @@
+import requests
+
+# does the request lib actually work
+print requests.__version__
+
+# doing https gives you SNIMissingWarning because installed python is old.
+response = requests.get("http://google.com")
+
+# gave 200 because it automatically follows redirects, unlike curl.
+# need to disable that manually if you dont want it to follow redirects.
+print response.status_code
+
+response2 = requests.post("http://ccid-eddieantonio.rhcloud.com/billy_the-kid")
+print response2.status_code
+print response2.content
+
+
